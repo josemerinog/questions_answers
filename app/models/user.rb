60d7f179babc4e7cc6917c56831a3f1f61c253ea
Jose Merino <josemerinog@gmail.com>
2018-05-17 11:06:16 -0500

    user controller y formulario parcial Registro Usuario

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+	# attr_accessor :username, :password, :password_confirmation
+	has_secure_password
+
+	validates :username, presence: true, uniqueness: {case_sensitive: false},
+								length: { in: 4..12},
+								format: { with: /\A[a-z][a-z0-9*\z]/, message: 'Solamente se permiten letras minúsculas y números.'}
+	validates :password, length: { in: 4..8 } 
+	validates :password_confirmation, length: { in: 4..8 } 
+end
