72e785ff1215c23d8173ea7582785e4e8b4f81cd
Jose Antonio Merino <josemerinog@gmail.com>
2018-05-21 20:47:48 -0500

    action SHOW y link para mostrar una pregunta

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,11 +11,17 @@ class QuestionsController < ApplicationController
 		@question = current_user.questions.new(question_params)
 		if @question.save
 			flash[:success] = 'Su pregunta ha sido publicada!'
-			redirect_to root_url
+			# redirect_to root_url
+			redirect_to @question 
 		else
 		  	@questions = Question.unsolved(params)
 			render 'index'
 		end
+	end
+
+	def show
+		@question = Question.find(params[:id])
+		
 	end
 
 private
